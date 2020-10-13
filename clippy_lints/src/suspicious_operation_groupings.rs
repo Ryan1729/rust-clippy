4ebfff2fc1166c25dc8408b28b78a3546880702d
Ryan1729 <Ryan1729@gmail.com>
2020-10-13 17:15:54 -0600

    start implementing `IdentDifference` creation

--- clippy_lints/src/suspicious_operation_groupings.rs
+++ clippy_lints/src/suspicious_operation_groupings.rs
@@ -1,14 +1,15 @@
-use core::iter::{self, FusedIterator};
+use crate::utils::{ast_utils::eq_id, snippet_with_applicability, span_lint_and_sugg};
+use core::iter::FusedIterator;
+use core::ops::{Add, AddAssign};
 use if_chain::if_chain;
-use crate::utils::{span_lint_and_sugg, snippet_with_applicability};
-use rustc_data_structures::fx::FxHashSet;
-use rustc_errors::Applicability;
-use rustc_lint::{EarlyLintPass, EarlyContext};
-use rustc_session::{declare_lint_pass, declare_tool_lint};
-use rustc_span::{Span};
-use rustc_span::symbol::Ident;
 use rustc_ast::ast::*;
 use rustc_ast::ptr::P;
+use rustc_data_structures::fx::FxHashSet;
+use rustc_errors::Applicability;
+use rustc_lint::{EarlyContext, EarlyLintPass};
+use rustc_session::{declare_lint_pass, declare_tool_lint};
+use rustc_span::symbol::Ident;
+use rustc_span::Span;
 
 declare_clippy_lint! {
     /// **What it does:**
@@ -53,12 +54,12 @@ impl EarlyLintPass for SuspiciousOperationGroupings {
 
             let mut paired_identifiers = FxHashSet::default();
 
-            for (i, BinaryOp{ left, right, .. }) in binops.iter().enumerate() {
-                match ident_difference(left, right) {
+            for (i, BinaryOp { left, right, .. }) in binops.iter().enumerate() {
+                match ident_difference_expr(left, right) {
                     IdentDifference::NoDifference => {
                         // The `eq_op` lint should catch this if it's an issue.
                         return;
-                    }
+                    },
                     IdentDifference::Single(ident_loc, ident) => {
                         one_ident_difference_count += 1;
                         if let Some(previous_expected) = expected_ident_loc {
@@ -72,16 +73,16 @@ impl EarlyLintPass for SuspiciousOperationGroupings {
                         }
 
                         paired_identifiers.insert(ident);
-                    }
+                    },
                     IdentDifference::Double(ident_loc1, ident_loc2) => {
                         double_difference_info = Some((i, ident_loc1, ident_loc2));
-                    }
+                    },
                     IdentDifference::Multiple | IdentDifference::NonIdentDifference => {
                         // It's too hard to know whether this is a bug or not.
                         // TODO: Do we need to recurse in order to find known
                         // buggy expressions inside complicated ones?
                         return;
-                    }
+                    },
                 }
             }
 
@@ -144,23 +145,18 @@ fn ident_swap_sugg(
         paired_identifiers.contains(&left_ident),
         paired_identifiers.contains(&right_ident),
     ) {
-        (true, true)|(false, false) => {
-            // We don't have a good guess of what ident should be 
+        (true, true) | (false, false) => {
+            // We don't have a good guess of what ident should be
             // used instead, in these cases.
             *applicability = Applicability::MaybeIncorrect;
 
             // We arbitraily choose one side to suggest changing,
-            // since we don't have a better guess. If the user 
+            // since we don't have a better guess. If the user
             // ends up duplicating a clause, the `logic_bug` lint
             // should catch it.
 
-            let right_suggestion = suggestion_with_swapped_ident(
-                cx,
-                &binop.right,
-                location,
-                left_ident,
-                applicability,
-            )?;
+            let right_suggestion =
+                suggestion_with_swapped_ident(cx, &binop.right, location, left_ident, applicability)?;
 
             format!(
                 "{} {} {}",
@@ -170,16 +166,11 @@ fn ident_swap_sugg(
             )
         },
         (false, true) => {
-            // We haven't seen a pair involving the left one, so 
+            // We haven't seen a pair involving the left one, so
             // it's probably what is wanted.
 
-            let right_suggestion = suggestion_with_swapped_ident(
-                cx,
-                &binop.right,
-                location,
-                left_ident,
-                applicability,
-            )?;
+            let right_suggestion =
+                suggestion_with_swapped_ident(cx, &binop.right, location, left_ident, applicability)?;
 
             format!(
                 "{} {} {}",
@@ -189,15 +180,9 @@ fn ident_swap_sugg(
             )
         },
         (true, false) => {
-            // We haven't seen a pair involving the right one, so 
+            // We haven't seen a pair involving the right one, so
             // it's probably what is wanted.
-            let left_suggestion = suggestion_with_swapped_ident(
-                cx,
-                &binop.left,
-                location,
-                right_ident,
-                applicability,
-            )?;
+            let left_suggestion = suggestion_with_swapped_ident(cx, &binop.left, location, right_ident, applicability)?;
 
             format!(
                 "{} {} {}",
@@ -222,11 +207,28 @@ fn chained_binops(kind: &ExprKind) -> Option<Vec<BinaryOp>> {
     todo!()
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
 struct IdentLocation {
     index: usize,
 }
 
+impl Add for IdentLocation {
+    type Output = IdentLocation;
+
+    fn add(self, other: Self) -> Self::Output {
+        match (self, other) {
+            _ => todo!(),
+        }
+    }
+}
+
+impl AddAssign for IdentLocation {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 enum IdentDifference {
     NoDifference,
     Single(IdentLocation, Ident),
@@ -235,15 +237,118 @@ enum IdentDifference {
     NonIdentDifference,
 }
 
-fn ident_difference(left: &Expr, right: &Expr) -> IdentDifference {
-    // This function cannot use IdentIter because it should return early
-    // if the expressions have any non-ident differences.
-    todo!()
+impl Add for IdentDifference {
+    type Output = IdentDifference;
+
+    fn add(self, other: Self) -> Self::Output {
+        match (self, other) {
+            _ => todo!(),
+        }
+    }
+}
+
+impl AddAssign for IdentDifference {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other
+    }
+}
+
+impl IdentDifference {
+    /// Returns true if learning about more differences will not change the value
+    /// of this `IdentDifference`, and false otherwise.
+    fn is_complete(&self) -> bool {
+        match self {
+            Self::NoDifference | Self::Single(_, _) | Self::Double(_, _) => false,
+            Self::Multiple | Self::NonIdentDifference => true,
+        }
+    }
+}
+
+fn ident_difference_expr(left: &Expr, right: &Expr) -> IdentDifference {
+    ident_difference_expr_with_base_location(left, right, IdentLocation::default()).0
+}
+
+fn ident_difference_expr_with_base_location(
+    left: &Expr,
+    right: &Expr,
+    mut base: IdentLocation,
+) -> (IdentDifference, IdentLocation) {
+    // Ideally, this function should not use IdentIter because it should return
+    // early if the expressions have any non-ident differences.
+    //
+    // But, we cannot (easily?) use a `rustc_ast::visit::Visitor`, since we need
+    // the two expressions to be walked in lockstep. And without a `Visitor`, we'd
+    // have to do all the AST traversal ourselves, which is a lot of work, since to
+    // do it properly we'd need to be able to handle more or less every possible
+    // AST node since `Item`s can be written inside `Expr`s.
+    //
+    // In practice, it seems likely that expressions, above a certain size, that
+    // happen to use the exact same idents in the exact same order, and which are
+    // not structured the same, would be rare. Therefore it seems likely that if
+    // we do only the first layer of matching ourselves and eventually fallback on
+    // IdentIter, then the output of this function will be almost always be correct
+    // in practice.
+    //
+    // If it turns out that problematic cases are more prelavent than we assume,
+    // then we should be able to change this function to do the correct traversal,
+    // without needing to change the rest of the code.
+    let mut difference = IdentDifference::NoDifference;
+
+    for (left_attr, right_attr) in left.attrs.iter().zip(right.attrs.iter()) {
+        let (new_difference, new_base) =
+            ident_difference_via_ident_iter_with_base_location(left_attr, right_attr, base);
+        base = new_base;
+        difference += new_difference;
+        if difference.is_complete() {
+            return (difference, base);
+        }
+    }
+
+    match (&left.kind, &right.kind) {
+        _ => todo!(),
+    }
+}
+
+fn ident_difference_via_ident_iter<Iterable: Into<IdentIter>>(left: Iterable, right: Iterable) -> IdentDifference {
+    ident_difference_via_ident_iter_with_base_location(left, right, IdentLocation::default()).0
+}
+
+fn ident_difference_via_ident_iter_with_base_location<Iterable: Into<IdentIter>>(
+    left: Iterable,
+    right: Iterable,
+    mut base: IdentLocation,
+) -> (IdentDifference, IdentLocation) {
+    // See the note in `ident_difference_expr_with_base_location` about `IdentIter`
+    let mut difference = IdentDifference::NoDifference;
+
+    let mut left_iterator = left.into();
+    let mut right_iterator = right.into();
+
+    loop {
+        base += IdentLocation { index: 1 };
+        match (left_iterator.next(), right_iterator.next()) {
+            (Some(left_ident), Some(right_ident)) => {
+                if eq_id(left_ident, right_ident) {
+                    continue;
+                }
+
+                difference += IdentDifference::Single(base, right_ident);
+                if difference.is_complete() {
+                    return (difference, base);
+                }
+            },
+            (Some(_), None) | (None, Some(_)) => {
+                return (IdentDifference::NonIdentDifference, base);
+            },
+            (None, None) => {
+                return (difference, base);
+            },
+        }
+    }
 }
 
 fn get_ident(expr: &Expr, location: IdentLocation) -> Option<Ident> {
-    IdentIter::new(expr)
-        .nth(location.index)
+    IdentIter::from(expr).nth(location.index)
 }
 
 fn suggestion_with_swapped_ident(
@@ -253,139 +358,38 @@ fn suggestion_with_swapped_ident(
     ident: Ident,
     applicability: &mut Applicability,
 ) -> Option<String> {
-    get_ident(expr, location)
-        .map(|current_ident| {
-            format!(
-                "{}{}{}",
-                snippet_with_applicability(
-                    cx,
-                    expr.span
-                        .with_hi(current_ident.span.lo()),
-                    "..",
-                    applicability
-                ),
-                current_ident.to_string(),
-                snippet_with_applicability(
-                    cx,
-                    expr.span
-                        .with_lo(current_ident.span.hi()),
-                    "..",
-                    applicability
-                ),
-            )
-        })
+    get_ident(expr, location).map(|current_ident| {
+        format!(
+            "{}{}{}",
+            snippet_with_applicability(cx, expr.span.with_hi(current_ident.span.lo()), "..", applicability),
+            current_ident.to_string(),
+            snippet_with_applicability(cx, expr.span.with_lo(current_ident.span.hi()), "..", applicability),
+        )
+    })
 }
 
-type Inner<'a> = Box<dyn Iterator<Item = <IdentIter<'a> as Iterator>::Item> + 'a>;
+// TODO replace this with a call to code from
+// https://github.com/rust-lang/rust-clippy/pull/6100 once that is merged.
+struct IdentIter {}
 
-struct IdentIter<'expr> {
-    expr: &'expr Expr,
-    inner: Option<Inner<'expr>>,
-    done: bool,
-}
-
-impl <'expr> IdentIter<'expr> {
-    fn new(expr: &'expr Expr) -> Self {
-        Self {
-            expr,
-            inner: None,
-            done: false,
-        }
-    }
-
-    /// This is a convenience method to help with type inference.
-    fn new_p(expr: &'expr P<Expr>) -> Self {
-        Self::new(expr)
-    }
-}
-
-impl <'expr> Iterator for IdentIter<'expr> {
+impl Iterator for IdentIter {
     type Item = Ident;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.done {
-            return None;
-        }
-
-        let inner_opt = &mut self.inner;
-
-        if let Some(mut inner) = inner_opt.take() {
-            let output = inner.next();
-
-            if output.is_some() {
-                *inner_opt = Some(inner);
-                return output;
-            }
-        }
-
-        macro_rules! set_and_call_next {
-            ($iter: expr) => {{
-                let mut p_iter = $iter;
-
-                let next_item = p_iter.next();
-
-                *inner_opt = Some(Box::new(p_iter));
-
-                next_item
-            }}
-        }
-
-        let output = match self.expr.kind {
-            ExprKind::Lit(_)|ExprKind::Err => None,
-            ExprKind::Path(_, ref path)
-            | ExprKind::MacCall(MacCall{ ref path, ..}) => {
-                set_and_call_next!(
-                    path.segments.iter()
-                        .map(|s| s.ident)
-                )
-            },
-            ExprKind::Box(ref expr)
-            | ExprKind::Unary(_, ref expr) => {
-                set_and_call_next!(
-                    IdentIter::new(expr)
-                )
-            },
-            ExprKind::Array(ref exprs)|ExprKind::Tup(ref exprs) => {
-                set_and_call_next!(
-                    exprs.iter()
-                        .flat_map(IdentIter::new_p)
-                )
-            },
-            ExprKind::Call(ref func, ref args) => {
-                set_and_call_next!(
-                    IdentIter::new(func)
-                        .chain(
-                            args.iter()
-                                .flat_map(IdentIter::new_p)
-                        )
-                )
-            },
-            ExprKind::MethodCall(ref method_name, ref args, _) => {
-                set_and_call_next!(
-                    iter::once(method_name.ident)
-                        .chain(
-                            args.iter()
-                                .flat_map(IdentIter::new_p)
-                        )
-                )
-            },
-            ExprKind::Binary(_, ref left, ref right) => {
-                set_and_call_next!(
-                    IdentIter::new(left)
-                        .chain(
-                            IdentIter::new(right)
-                        )
-                )
-            },
-            _ => todo!(),
-        };
-
-        if output.is_none() {
-            self.done = true;
-        }
-
-        output
+        todo!()
     }
 }
 
-impl <'expr> FusedIterator for IdentIter<'expr> {}
+impl FusedIterator for IdentIter {}
+
+impl From<&Expr> for IdentIter {
+    fn from(expr: &Expr) -> Self {
+        todo!()
+    }
+}
+
+impl From<&Attribute> for IdentIter {
+    fn from(attr: &Attribute) -> Self {
+        todo!()
+    }
+}
